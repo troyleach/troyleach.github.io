d0b0a12a01f7c65e8b0da82edb9e523fd9108dfc
Troy Leach <troyleach29@gmail.com>
2020-11-02 21:28:20 -0600

    finished the work I have done page

--- script/displayImage.js
+++ script/displayImage.js
@@ -1,14 +1,27 @@
 const displayWorkImages = ['bluecrew.png', 'canceriq1.png', 'divegalaxsea.png', 'divegalaxseaAdmin.png']
 const jobImagesBc = ['bc1.png', 'bc2.png', 'bc3.png', 'bc4.png', 'bc5.png', 'bc6.png']
 const jobImagesCiq = ['ciq1.jpg', 'ciq2.jpg', 'ciq3.jpg', 'ciq4.jpg', 'ciq5.jpg', 'ciq6.png']
+const jobImagesDga = ['dga1.png', 'dga2.png', 'dga3.png']
 const COMPANY_INFORMATION = {
   'bluecrew': {
     'name': "Bluecrew",
-    'lang': "Backend built with NodeJS, Express, Front End built with React, Redux"
+    'lang': "Backend built with NodeJS, Express, Front End built with React, Redux",
+    'description': ""
   },
   'canceriq1': {
     'name': "Cancer IQ",
-    'lang': "Built with Ruby on Rails, Microservice built with NodeJS, Express"
+    'lang': "Built with Ruby on Rails, Microservice built with NodeJS, Express",
+    'description': ""
+  },
+  'divegalaxseaAdmin': {
+    'name': "Dive Galaxsea Admin panel",
+    'lang': "Built with Angular 1.x",
+    'description': "Allows the user to change pricing. Consumes Rails API."
+  },
+  'divegalaxsea': {
+    'name': "Dive Galaxsea web site",
+    'lang': "Built with Angular 1.x",
+    'description': "Very first web site I built that is in production.\n This is site is getting rebuilt using react. Consumes Rails API."
   }
 }
 
@@ -47,29 +60,36 @@ function buildModal(company) {
   let jobImages;
   console.log('compaing now', companyName)
   if (companyName[0] === 'bluecrew') {
-    console.log('bluecrew.png conditional')
     jobImages = jobImagesBc
-  } else {
-    console.log('cancier IQ.png conditional')
+  } else if (companyName[0] === 'canceriq1') {
     jobImages = jobImagesCiq;
+  } else if (companyName[0] === 'divegalaxseaAdmin') {
+    jobImages = jobImagesDga;
+  } else if (companyName[0] === 'divegalaxsea') {
+    window.open('http://divegalaxsea.com', '_blank');
   }
   const title = document.getElementById('exampleModalLabel');
   title.setAttribute('class', 'modal-title')
   title.innerText = COMPANY_INFORMATION[companyName].name;
 
   const description = document.getElementById('modalDescription')
+  const secondDescription = document.getElementById('modalSecondDescription')
   description.innerText = COMPANY_INFORMATION[companyName].lang;
-  // get the images for the company hardcoded for now 
-  for (let i = 0; i < jobImages.length; i++) {
-    const img = document.createElement('img');
-    const div = document.createElement('div');
+  secondDescription.innerText = COMPANY_INFORMATION[companyName].description;
 
-    div.setAttribute('class', 'item')
-    img.setAttribute('class', 'bluecrew-img')
-    img.setAttribute('src', `images/${jobImages[i]}`)
-    div.append(img);
-    ModalImages.append(div);
-  };
+  // get the images for the company hardcoded for now 
+  console.log('images', jobImages)
+  if (jobImages)
+    for (let i = 0; i < jobImages.length; i++) {
+      const img = document.createElement('img');
+      const div = document.createElement('div');
+
+      div.setAttribute('class', 'item')
+      img.setAttribute('class', 'bluecrew-img')
+      img.setAttribute('src', `images/${jobImages[i]}`)
+      div.append(img);
+      ModalImages.append(div);
+    };
 };
 
 function getCompany(company) { };
